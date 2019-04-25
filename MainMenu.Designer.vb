29f5f9b14cf43d1cad5b95f2cadb15be7ec39efa
Rabih Samara <rsamara61@hotmail.com>
2019-04-25 12:19:28 -0400

    April252019 - 01

--- MainMenu.Designer.vb
+++ MainMenu.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,18 +20,54 @@ Partial Class MainMenu
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.MenuStrip1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1480, 830)
+        Me.SplitContainer1.SplitterDistance = 32
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1480, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
         '
         'MainMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1480, 830)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "MainMenu"
         Me.Text = "MainMenu"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents MenuStrip1 As MenuStrip
 End Class
--- MainMenu.vb
+++ MainMenu.vb
@@ -1,8 +1,58 @@
 ﻿Public Class MainMenu
 
     Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Text = GlobalVariables.Gl_LogUserID & " - " & GlobalVariables.Gl_Company
+
+        Me.Text = GlobalVariables.Gl_Company & " - (" & GlobalVariables.Gl_LogUserID & ")"
+        BuildMenu()
 
     End Sub
+
+    Private Sub BuildMenu()
+
+        Dim ms As New MenuStrip
+        ms.Parent = Me
+
+        'File Menu
+        Dim fileItem As New ToolStripMenuItem("&File")
+        Dim exitItem As New ToolStripMenuItem("&Exit", Nothing, New EventHandler(AddressOf OnExit))
+
+        exitItem.ShortcutKeys = Keys.Control Or Keys.X
+        fileItem.DropDownItems.Add(exitItem)
+
+        ms.Items.Add(fileItem)
+
+        'Edit Menu
+        Dim EditItem As New ToolStripMenuItem("E&dit")
+
+        ms.Items.Add(EditItem)
+
+        'Utilities Menu
+        Dim UtilItem As New ToolStripMenuItem("&Utilities")
+
+        ms.Items.Add(UtilItem)
+
+        'Reports menu
+        Dim RepItem As New ToolStripMenuItem("&Reports")
+
+        ms.Items.Add(RepItem)
+
+        '******************************************************************
+        'Move to form menustrip.
+        '******************************************************************
+        MenuStrip1 = ms
+
+    End Sub
+
+    '*********************************************************************
+    'Event handlers for items listed under Menu File
+    '*********************************************************************
+    Private Sub OnExit(ByVal sender As Object, ByVal e As EventArgs)
+        Application.Exit()
+    End Sub
+
+
+
+
+
 
 End Class
