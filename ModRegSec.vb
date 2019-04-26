878c07775399a23c9c8a2242bb3ce10d16d542d1
Rabih Samara <rsamara61@hotmail.com>
2019-04-26 13:52:01 -0400

    april26,2019 04

--- ModRegSec.vb
+++ ModRegSec.vb
@@ -12,17 +12,36 @@ Module ModRegSec
         Dim S As Integer = 0
         Dim MenuMItem As String = ""
         Dim menuSubItem As String = ""
+        Dim tsql As String = ""
 
         Do While I < inmenu.Items.Count
             MenuMItem = Replace(inmenu.Items(I).Text, "&", "") ' get top menu items File, edit...
             MsgBox(MenuMItem)
+
+            tsql = "If Not Exists(select 1 from MenuDfltSecurity where MenuMItem = '" & MenuMItem & "') "
+            tsql = tsql & "Begin insert into MenuDfltSecurity (MenuMItem, MenuSitem, MenuS2Item, MenuSecLevel, MenuActive) values ('" & MenuMItem & "','','',0,1) "
+            tsql = tsql & "End"
+            GlobalVariables.Gl_SQLStr = tsql
+            If (ExecuteSqlTransaction(GlobalVariables.Gl_ConnectionSTR) = False) Then
+                MsgBox("error wrting security menus!")
+                RegisterMenu = False
+                Exit Function
+            End If
 
             Dim tmpMenuItem As ToolStripMenuItem = inmenu.Items(I)
             S = 0
             Do While S < tmpMenuItem.DropDownItems.Count
                 menuSubItem = Replace(tmpMenuItem.DropDownItems(S).Text, "&", "")
                 MsgBox(menuSubItem)
-
+                tsql = "If Not Exists(select 1 from MenuDfltSecurity where MenuMItem = '" & MenuMItem & "' and MenuSitem = '" & menuSubItem & "') "
+                tsql = tsql & "Begin insert into MenuDfltSecurity (MenuMItem, MenuSitem, MenuS2Item, MenuSecLevel, MenuActive) values ('" & MenuMItem & "','" & menuSubItem & "','',0,1) "
+                tsql = tsql & "End"
+                GlobalVariables.Gl_SQLStr = tsql
+                If (ExecuteSqlTransaction(GlobalVariables.Gl_ConnectionSTR) = False) Then
+                    MsgBox("error writing security sub menu of" & MenuMItem)
+                    RegisterMenu = False
+                    Exit Function
+                End If
 
                 S = S + 1
             Loop
