89e5500b984e78c618ccc1d6c83d6de6d9bbe50d
Rabih Samara <rsamara61@hotmail.com>
2019-05-03 16:03:50 -0400

    may022019 - 06

--- AppLocksScreen.Designer.vb
+++ AppLocksScreen.Designer.vb
@@ -22,9 +22,42 @@ Partial Class AppLocksScreen
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.DataGridLocks = New System.Windows.Forms.DataGridView()
+        Me.cmdExit = New System.Windows.Forms.Button()
+        CType(Me.DataGridLocks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridLocks
+        '
+        Me.DataGridLocks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridLocks.Location = New System.Drawing.Point(3, 33)
+        Me.DataGridLocks.Name = "DataGridLocks"
+        Me.DataGridLocks.Size = New System.Drawing.Size(945, 331)
+        Me.DataGridLocks.TabIndex = 0
+        '
+        'cmdExit
+        '
+        Me.cmdExit.Location = New System.Drawing.Point(3, 3)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(81, 24)
+        Me.cmdExit.TabIndex = 1
+        Me.cmdExit.Text = "Exit"
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'AppLocksScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(952, 375)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Controls.Add(Me.DataGridLocks)
+        Me.Name = "AppLocksScreen"
         Me.Text = "AppLocksScreen"
+        CType(Me.DataGridLocks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents DataGridLocks As DataGridView
+    Friend WithEvents cmdExit As Button
 End Class
--- AppLocksScreen.vb
+++ AppLocksScreen.vb
@@ -1,3 +1,37 @@
-﻿Public Class AppLocksScreen
+﻿Imports System.Data.SqlClient
+
+Public Class AppLocksScreen
+    Dim sCommand As SqlCommand
+    Dim sAdapter As SqlDataAdapter
+    Dim sBuilder As SqlCommandBuilder
+    Dim sDs As DataSet
+    Dim sTable As DataTable
+
+    Private Sub AppLocksScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadAppLocks()
+    End Sub
+
+    Private Sub CmdExit_Click(sender As Object, e As EventArgs) Handles cmdExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub LoadAppLocks()
+        Dim sql As String = "SELECT ID,Userid,Formname,ctrlname,ctrlvalue,ctrlopert,lockeddate FROM AppLocks order by Userid asc,Formname asc,ctrlname asc"
+        Using connection As New SqlConnection(GlobalVariables.Gl_ConnectionSTR)
+            connection.Open()
+            sCommand = New SqlCommand(sql, connection)
+            sAdapter = New SqlDataAdapter(sCommand)
+            sBuilder = New SqlCommandBuilder(sAdapter)
+            sDs = New DataSet()
+            sAdapter.Fill(sDs, "AppLocks")
+            sTable = sDs.Tables("AppLocks")
+            connection.Close()
+            DataGridLocks.DataSource = sDs.Tables("AppLocks")
+            DataGridLocks.ReadOnly = True
+            DataGridLocks.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            DataGridLocks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+        End Using
+
+    End Sub
 
 End Class
