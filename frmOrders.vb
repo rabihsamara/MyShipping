ac913ebeecfb7d53284ed299f597cc96a0a0ad15
Rabih Samara <rsamara61@hotmail.com>
2019-05-12 01:58:05 -0400

    May122019 - 01

--- frmOrders.vb
+++ frmOrders.vb
@@ -7,6 +7,8 @@ Public Class frmOrders
     Private sBuilder As SqlCommandBuilder
     Private sDs As DataSet
     Private sTable As DataTable
+    Private myReader As SqlDataReader
+    Private Ordrecord As Orders = New Orders()
 
     Private Sub FrmOrders_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -21,7 +23,14 @@ Public Class frmOrders
         OrderNO.Enabled = False
 
         If (GlobalVariables.Gl_OrdCallFrmID = "COE") Then 'customer screen Existing order
-            LoadOrdToScreen("orders")
+            GlobalVariables.Gl_SQLStr = "SELECT ID,CustNo,AccountNo,OrderNO,ordStat,ordshipID,SHName,SHadd1,SHadd2,cmbSHCity,SHPcode,cmbSHProv,cmbSHCountry,"
+            GlobalVariables.Gl_SQLStr = GlobalVariables.Gl_SQLStr & "BLName,BLadd1,BLadd2,cmbBLcity,BLpcode,cmbBLProv,cmbBLCountry,CONVERT(date,datecreated) as datecreated,CONVERT(date,dateupdated) as dateupdated,CreatedBy"
+            GlobalVariables.Gl_SQLStr = GlobalVariables.Gl_SQLStr & " FROM orders where  CustNo = '" & GlobalVariables.Gl_tmpcustid & "' and AccountNo  = '" & GlobalVariables.Gl_tmpacctname & "' and OrderNO = " & GlobalVariables.Gl_SelOrder
+            If (ReadOrder() = False) Then
+                MsgBox("Error reading Order !")
+                Exit Sub
+            End If
+            LoadOrdToScreen()
         ElseIf (GlobalVariables.Gl_OrdCallFrmID = "CON") Then 'customer screen new order
 
         ElseIf (GlobalVariables.Gl_OrdCallFrmID = "COM") Then 'menu
@@ -48,27 +57,84 @@ Public Class frmOrders
     End Sub
 #End Region
 
+#Region "ReadORDintoObject"
+
+    Public Function ReadOrder() As Boolean
+
+        GlobalVariables.GL_Stat = False
+        ReadOrder = False
+
+        Using mysqlConn As New SqlConnection(GlobalVariables.Gl_ConnectionSTR)
+            Try
+
+                sCommand = mysqlConn.CreateCommand
+                sCommand.CommandText = GlobalVariables.Gl_SQLStr
+                mysqlConn.Open()
+
+                myReader = sCommand.ExecuteReader()
+                Do While myReader.Read()
+                    Ordrecord.MyID = myReader.GetValue(0)
+                    Ordrecord.MyCustNo = myReader.GetString(1).ToString
+                    Ordrecord.MyAccountNo = myReader.GetString(2).ToString
+                    Ordrecord.MyOrderNO = myReader.GetValue(3)
+                    Ordrecord.MyordStat = myReader.GetString(4).ToString
+                    Ordrecord.MyorshipID = myReader.GetString(5).ToString
+                    Ordrecord.MySHName = myReader.GetString(6).ToString
+                    Ordrecord.MySHadd1 = myReader.GetString(7).ToString
+                    Ordrecord.MySHadd2 = myReader.GetString(8).ToString
+                    Ordrecord.MycmbSHCity = myReader.GetString(9).ToString
+
+                    Ordrecord.MySHPcode = myReader.GetString(10).ToString
+                    Ordrecord.MycmbSHProv = myReader.GetString(11).ToString
+                    Ordrecord.MycmbSHCountry = myReader.GetString(12).ToString
+                    Ordrecord.MyBLName = myReader.GetString(13).ToString
+                    Ordrecord.MyBLadd1 = myReader.GetString(14).ToString
+                    Ordrecord.MyBLadd2 = myReader.GetString(15).ToString
+                    Ordrecord.MycmbBLcity = myReader.GetString(16).ToString
+
+                    Ordrecord.MyBLpcode = myReader.GetString(17).ToString
+                    Ordrecord.MycmbBLProv = myReader.GetString(18).ToString
+                    Ordrecord.MycmbBLCountry = myReader.GetString(19).ToString
+                    Ordrecord.Mydatecreated = myReader.GetDateTime(20)
+                    Ordrecord.Mydateupdated = myReader.GetDateTime(21)
+                    Ordrecord.MyCreatedBy = myReader.GetString(22).ToString
+                    ReadOrder = True
+                Loop
+
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+                ReadOrder = False
+            Finally
+                If Not (myReader Is Nothing) Then
+                    myReader.Close()
+                End If
+                If Not (mysqlConn Is Nothing) Then
+                    mysqlConn.Close()
+                End If
+            End Try
+        End Using
+
+    End Function
+
+#End Region
+
 #Region "ReadOrdinfotoscreen"
 
     '****************************************************************************************
     '* Load data to screen
     '****************************************************************************************
-    Private Function LoadOrdToScreen(ByVal crtable As String) As Boolean
+    Private Function LoadOrdToScreen() As Boolean
 
-        Dim sql As String
-        sql = "SELECT ID,CustNo,AccountNo,OrderNO,ordStat,ordshipID,SHName,SHadd1,SHadd2,cmbSHCity,SHPcode,cmbSHProv,cmbSHCountry,"
-        sql = sql & "BLName,BLadd1,BLadd2,cmbBLcity,BLpcode,cmbBLProv,cmbBLCountry,CONVERT(date,datecreated) as datecreated,CONVERT(date,dateupdated) as dateupdated,CreatedBy"
-        sql = sql & " FROM " & crtable & " where  CustNo = '" & GlobalVariables.Gl_tmpcustid & "' and AccountNo  = '" & GlobalVariables.Gl_tmpacctname & "' and OrderNO = " & GlobalVariables.Gl_SelOrder
         LoadOrdToScreen = False
 
         Using connection As New SqlConnection(GlobalVariables.Gl_ConnectionSTR)
             connection.Open()
-            sCommand = New SqlCommand(sql, connection)
+            sCommand = New SqlCommand(GlobalVariables.Gl_SQLStr, connection)
             sAdapter = New SqlDataAdapter(sCommand)
             sBuilder = New SqlCommandBuilder(sAdapter)
             sDs = New DataSet()
-            sAdapter.Fill(sDs, crtable)
-            sTable = sDs.Tables(crtable)
+            sAdapter.Fill(sDs, "Orders")
+            sTable = sDs.Tables("Orders")
 
             If sTable.Rows.Count > 0 Then
                 ControlsRecr(Me.Controls)
